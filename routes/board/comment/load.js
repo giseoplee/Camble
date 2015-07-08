39035e9a50aa62b8d317885a432a27c994f87d8e
giseoplee <llgs901@naver.com>
2015-07-09 01:54:28 +0900

    w
    w

--- routes/board/comment/load.js
+++ routes/board/comment/load.js
@@ -13,17 +13,12 @@ var connection = mysql.createConnection({
 
 router.post('/',function(req, res, next){
 
-	connection.query("update univ_"+req.body.sc_code+"_board set view_count = view_count+1 where id=?;",
-		[req.body.univ_board_id],function(error, info){
-			if(error==null){
-				connection.query("select camble_user_id, comment_writer, comment_content, likes_count, updated_at from univ_"+req.body.sc_code+"_board_comment where univ_"+req.body.sc_code+"_board_id=? order by id desc;",
-					[req.body.univ_board_id], function(error, cursor){
-						if(cursor.length > 0){
-							res.status(200).json(cursor);
-						}else{
-							res.status(200).json({message : "Null Comment"});
-						}
-					});
+	connection.query("select camble_user_id, comment_writer, comment_content, likes_count, updated_at from univ_"+req.body.sc_code+"_board_comment where univ_"+req.body.sc_code+"_board_id=? order by id desc;",
+		[req.body.univ_board_id], function(error, cursor){
+			if(cursor.length > 0){
+				res.status(200).json(cursor);
+			}else{
+				res.status(200).json({message : "Null Comment"});
 			}
 	});
 });
