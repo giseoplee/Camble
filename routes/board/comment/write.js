99f5a9cc3b4f46b6b10028d089e5dbe823ebb73c
giseoplee <llgs901@naver.com>
2015-07-09 16:34:15 +0900

    q
    q

--- routes/board/comment/write.js
+++ routes/board/comment/write.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
     host : 'appjam.cyjao5zjyirq.us-west-2.rds.amazonaws.com'
 });
 
-router.post('/', function(req, res, next) { // sc_code , user_auth_key, comment_content, univ_board_id
+router.post('/', function(req, res, next) { 
 
     connection.query("select school.id,user.user_nickname from camble_schools as school inner join camble_users as user on school.id=user.camble_school_id where school.sc_code=? and user.id=?;",
         [req.body.sc_code, req.body.user_auth_key], function(error, cursor){
@@ -22,7 +22,6 @@ router.post('/', function(req, res, next) { // sc_code , user_auth_key, comment_
                     [req.body.univ_board_id, req.body.user_auth_key, cursor[0].user_nickname, req.body.comment_content], function(error, info){
                         console.log(query);
                         if(error==null){
-                            console.log("Comment Write Success");
                             logWrite(cursor[0].id, req.body.user_auth_key, req.body.univ_board_id, info.insertId, req.body.sc_code);
                         }else{
                             res.status(503).json({message : "Comment Write Fail"});
@@ -38,18 +37,14 @@ router.post('/', function(req, res, next) { // sc_code , user_auth_key, comment_
         connection.query("insert log_camble_writer set camble_school_id=?, camble_user_id=?, camble_univ_board_id=?, camble_univ_board_comment_id=?, created_at=now();",
             [sc_id, user_id, board_id, comment_id], function(error , info){
                 if(error==null){
-                    //res.status(200).json({message : "Comment Log Write Success"});
                     connection.query("update univ_"+sc_code+"_board set comment_count = comment_count+1 where id=?;", [board_id], function(error, info){
                         if(error==null){
-                            console.log("Comment Write Full Success");
                             res.status(200).json({message : "Comment Write Success"});    
                         }else{
-                            console.log("board table count update fail");
                             res.status(503).json({message : "board table count update fail"});
                         }
                     });
                 }else{
-                    console.log("Comment Log Write Fail");
                     res.status(503).json({message : "Comment Log Write Fail"});
                 }
             });
