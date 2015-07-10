58c91055a49e7fa3dac2b3a75b55746626e426ba
giseoplee <llgs901@naver.com>
2015-07-10 18:30:27 +0900

    ㅂ

--- routes/board/content/like.js
+++ routes/board/content/like.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
     'database':'camble'
 });
 
-router.post('/increment', function(req, res, next){ //sc_code : "학교 코드", user_auth_key : "사용자 키", univ_board_id : "게시판 글 번호"
+router.post('/increment', function(req, res, next){
 
 	connection.query("insert univ_"+req.body.sc_code+"_likes set content_id=?, camble_user_id=?, content_sort=1, created_at=now();",
 		[req.body.univ_board_id, req.body.user_auth_key], function(error, info){
@@ -19,13 +19,13 @@ router.post('/increment', function(req, res, next){ //sc_code : "학교 코드",
 				connection.query("update univ_"+req.body.sc_code+"_board set likes_count = likes_count+1 where id=?;", 
 				[req.body.univ_board_id], function(error, info){
 					if(error==null){
-						var query = connenction.query("update camble_integration_board set likes_count = likes_count+1 where sc_code=? and univ_board_id=?;",
-							[req.body.s_code, req.body.univ_board_id], function(error, info){
+						var query = connection.query("update camble_integration_board set likes_count = likes_count+1 where camble_school_code=? and univ_board_id=?;",
+							[req.body.sc_code, req.body.univ_board_id], function(error, info){
 								console.log(query);
 								if(error==null){
 									res.status(200).json({message : "Like Increment all Success"});		
 								}else{
-									res.status(503).json({message : "Like Count to Integration Board Update Fail"});		
+									res.status(503).json({message : "Like Count to Integration Board Update Fail(INC)"});		
 								}
 							});
 					}else{
@@ -47,12 +47,13 @@ router.post('/decrement', function(req, res, next){
 				connection.query("update univ_"+req.body.sc_code+"_board set likes_count = likes_count-1 where id=?;", 
 				[req.body.univ_board_id], function(error, info){
 					if(error==null){
-						connenction.query("update camble_integration_board set likes_count = likes_count-1 where sc_code=? and univ_board_id=?;",
-							[req.body.s_code, req.body.univ_board_id], function(error, info){
+						var query = connection.query("update camble_integration_board set likes_count = likes_count-1 where camble_school_code=? and univ_board_id=?;",
+							[req.body.sc_code, req.body.univ_board_id], function(error, info){
+								console.log(query);
 								if(error==null){
 									res.status(200).json({message : "Like Decrement all Success"});		
 								}else{
-									res.status(503).json({message : "Like Count to Integration Board Update Fail"});		
+									res.status(503).json({message : "Like Count to Integration Board Update Fail(DEC)"});		
 								}
 							});
 					}else{
